9090c0d949b345c0d2067c068ff8f12a07f1c8b3
Mdsujansarkar <sujansarkar786@gmail.com>
2021-06-05 11:56:35 +0600

    slider add

--- js/main.js
+++ js/main.js
@@ -33,3 +33,26 @@ $(window).on("scroll resize", function () {
   $(".overlay").height(window_offset);
   $(".caption").css("bottom", window_offset / 4);
 });
+
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
